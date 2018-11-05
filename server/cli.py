650ddf65819e34ccfcb61a37960b7a11ce6e5ab1
Byte_Predator <mattia_paris@hotmail.it>
2018-11-06 00:39:05 +0100

    added challenge update functionality, added name check when inserting a new challenge

--- server/cli.py
+++ server/cli.py
@@ -9,6 +9,13 @@ def insertChallenge(name, address, description, flag, base_points, blood_points)
     con = sql.connect(DATABASE_PATH)
     cur = con.cursor()
     cur.execute(
+        "SELECT id FROM challenges WHERE name = ?",
+        (
+            str(name),
+        )
+    )
+    assert (cur.rowcount == 0), "challenge {} already exists".format(name)
+    cur.execute(
         "INSERT INTO challenges(name, address, description, flag, base_points, blood_points) \
          VALUES (?, ?, ?, ?, ?, ?)",
         (
@@ -22,6 +29,38 @@ def insertChallenge(name, address, description, flag, base_points, blood_points)
     )
     con.commit()
 
+def updateChallenge(name, address, description, flag, base_points, blood_points):
+    con = sql.connect(DATABASE_PATH)
+    cur = con.cursor()
+    cur.execute(
+        "UPDATE challenges SET address = ?, description = ?, flag = ?, base_points = ?, blood_points = ? \
+         WHERE name=?",
+        (
+            str(address),
+            str(description),
+            hashlib.md5(flag.encode('utf8')).hexdigest(),
+            int(base_points),
+            int(blood_points),
+            str(name),
+        )
+    )
+    con.commit()
+
+def getChallengeByName(name):
+    con = sql.connect(DATABASE_PATH)
+    cur = con.cursor()
+    cur.execute(
+        "SELECT address, description, flag, base_points, blood_points FROM challenges\
+         WHERE name=?",
+        (
+            str(name),
+        )
+    )
+    row = cur.fetchone();
+    if (row == None):
+        raise Exception("challenge {} not found".format(name))
+    else:
+        return row
 
 def resetRank():
     con = sql.connect(DATABASE_PATH)
@@ -43,12 +82,15 @@ def main():
     parser.add_argument('--insert', action='store_true', default=False,
                     dest='insert',
                     help='Insert a new challenge.')
+    parser.add_argument('--update', action='store_true', default=False,
+                    dest='update',
+                    help='Update the challenge with the given name.')
     parser.add_argument('--reset', action='store_true', default=False,
                     dest='reset',
                     help='Drop the rank.')
     args = parser.parse_args()
     if (args.insert):
-        if(args.name and args.address and args.description and args.flag):
+        if(args.name and args.address and args.description and args.flag and args.base_points and args.blood_points):
             name = args.name.strip()
             address = args.address.strip()
             description = args.description.strip()
@@ -57,7 +99,23 @@ def main():
             blood_points = args.blood_points.strip()
             insertChallenge(name, address, description, flag, base_points, blood_points)
         else:
-            print "Please enter --name, --address, --description, --flag"
+            print "Please enter --name, --address, --description, --flag, --base-points, --blood-points"
+    if (args.update):
+        if(args.name):
+            (address, description, flag, base_points, blood_points) = getChallengeByName(args.name)
+            if(args.address):
+                address = args.address.strip()
+            if(args.description):
+                description = args.description.strip()
+            if(args.flag):
+                flag = args.flag.strip()
+            if(args.base_points):
+                base_points = args.base_points.strip()
+            if(args.blood_points):
+                blood_points = args.blood_points.strip()
+            updateChallenge(args.name, address, description, flag, base_points, blood_points)
+        else:
+            print "Please enter the name of the challenge to update"
     elif (args.reset):
         resetRank()
 
